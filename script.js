4df0921128fdac495b6d3899950555ee158a2355
arjinhaval2012-beep <arjinhaval2012@gmail.com>
2026-06-29 19:31:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// DARK MODE TOGGLE
+const toggle = document.getElementById("darkToggle");
 
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// SIMPLE SEARCH FUNCTION
+const searchInput = document.querySelector(".navbar input");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("keyup", () => {
+  let value = searchInput.value.toLowerCase();
+
+  cards.forEach(card => {
+    let text = card.textContent.toLowerCase();
+
+    if (text.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
